cccdddf55bfd3d1dc91f9abf92b00b62d1954857
lxstinthesky <manny.manatee12@gmail.com>
2021-08-09 11:33:24 +0200

    Initial Commit

---
+++ cmake-build-debug/CMakeFiles/nasto.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nasto.dir/main.cpp.obj"
+  "libnasto.dll.a"
+  "nasto.exe"
+  "nasto.exe.manifest"
+  "nasto.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/nasto.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/nasto.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for nasto.
+# This may be replaced when dependencies are built.
