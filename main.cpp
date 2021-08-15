ae2ecfc619c933570e02d90e8919989a6e9a8605
lxstinthesky <manny.manatee12@gmail.com>
2021-08-15 11:34:26 +0200

    Basic structure for program

--- main.cpp
+++ main.cpp
@@ -2,11 +2,20 @@
 #include <cmath>
 #include <fstream>
 
+/*
+Solving navier-stokes-equation as mentioned in Demtröder 1 p. 227 equ. 8.35a
+*/
 
 using namespace std;
+
+//global variable declarations here
+
+//function declarations here
 
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
+
+//auxiliary function implementation here
