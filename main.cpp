975bc8be1db9c9abafdf24faad211629d4b26611
lxstinthesky <manny.manatee12@gmail.com>
2021-08-27 16:33:52 +0200

    first attempt in spiral form

--- main.cpp
+++ main.cpp
@@ -16,59 +16,75 @@ using namespace std;
 
 //global variable declarations here
 
-double psi[NUM][NUM];
+//double psi[NUM + 1][NUM + 1];
+double error , psi[NUM + 1][NUM + 1]={{0}} , psin[NUM + 1][NUM + 1];
 
 //function declarations here
 void init();
+void jacobi();
+
+
 ofstream File;
 
 int main() {
 
     File.open("data.dat");
     init();
-    for(int n = 0; n < NUM/2; n++){
-        int i = n;
-        int j = n;
-        for(i = i + 1; i< NUM-n-1; i++){
-            psi[i+1][j] = 2 * psi[i][j] - psi[i-1][j];
-            File << i << ";" << j << ";" << psi[i+1][j] << endl;
-        }
-        for(j = j + 1; j < NUM-n-1; j++){
-            psi[i][j+1] = 2 * psi[i][j] - psi[i][j-1];
-            File << i << ";" << j << ";" << psi[i][j+1] << endl;
-        }
-        for(; i > n+1; i--){
-            psi[i-1][j] = 2 * psi[i][j] - psi[i+1][j];
-            File << i << ";" << j << ";" << psi[i-1][j] << endl;
-        }
-        for(; j > n+1; j--){
-            psi[i][j-1] = 2 * psi[i][j] - psi[i][j+1];
-            File << i << ";" << j << ";" << psi[i][j-1] << endl;
-        }
-    }
+
+    jacobi();
+
+    File.close();
+
     return 0;
 }
 
 //auxiliary function implementation here
 void init(){
-    int i;
-    //setting bottom edge
-    for(i = 0; i < NUM; i++){
-        psi[i][0] = 1;
-        File << i << ";" << 0 << ";" << psi[i][0] << endl;
+    int i, j;
+    for(i = 0; i < NUM+1; i++){
+        for(j = 0; j < NUM+1; j++){
+            psi[i][j] = 0;
+        }
     }
-    //setting right edge
-    for(i = 0; i < NUM; i++){
-        psi[NUM-1][i] = 0;
-        File << NUM-1 << ";" << i << ";" << psi[NUM-1][i] << endl;
-    }
-    //setting upper edge
-    for(i = 0; i < NUM; i++){
-        psi[i][NUM-1] = 1;
-        File << i << ";" << NUM-1 << ";" << psi[i][NUM-1] << endl;
-    }
-    //setting left edge
-    for(i = 0; i < NUM; i++){
+
+    //setting right and left edge
+    for(i = 0; i < NUM+1; i++){
+        psi[NUM][i] = 0;
+        psi[0][i] = 0;
+
+        File << NUM << ";" << i << ";" << psi[NUM][i] << endl;
         File << 0 << ";" << i << ";" << psi[0][i] << endl;
+    }
+    //setting bottom and upper edge
+    for(i = 0; i < NUM+1; i++) {
+        psi[i][0] = 1;
+        psi[i][NUM] = 1;
+
+        File << i << ";" << 0 << ";" << psi[i][0] << endl;
+        File << i << ";" << NUM << ";" << psi[i][NUM] << endl;
+    }
+}
+
+void jacobi () {
+    int i, j, k;
+    do {
+        error = 0.0;
+        for (i = 1; i < NUM; i++) {
+            for (j = 1; j < NUM; j++) {
+                psin[i][j] = 1.0/4.0*(psi[i + 1][j] + psi[i - 1][j] + psi[i][j + 1] + psi[i][j - 1]);
+                error += abs(psin[i][j] - psi[i][j]);
+            }}
+        for (i = 1; i < NUM; i++) {
+            for (j = 1; j < NUM; j++) {
+                psi[i][j] = psin[i][j];
+            }}
+        error /= NUM*NUM;
+        k++;
+    } while (error > 1.e-9 && k < 1000);
+
+    for(i = 0; i < NUM+1; i++){
+        for(j = 0; j < NUM+1; j++){
+            File << i << ";" << j << ";" << psin[i][j] << endl;
+        }
     }
 }
